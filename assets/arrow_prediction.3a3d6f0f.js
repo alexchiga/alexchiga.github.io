aed5abbc808fde1c819faa5af113c803bd1c51d9
Alex Chiga <chiga@protonmail.com>
2022-05-23 13:29:30 +0300

    upd

--- assets/arrow_prediction.3a3d6f0f.js
+++
@@ -1,1 +0,0 @@
-var e=(r,t)=>{const a=r.__vccOpts||r;for(const[o,s]of t)a[o]=s;return a},c="/assets/arrow_prediction.68649e5c.svg";export{e as _,c as a};
---
+++ assets/arrow_prediction.5bea0186.js
@@ -0,0 +1,1 @@
+var r="/assets/arrow_prediction.68649e5c.svg";export{r as a};
